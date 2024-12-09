669f16e9d1bff4539ca147ab5d6b0217c5a9d865
Alim <alim-malikov@mail.ru>
2024-12-09 20:38:25 +0300

    modified

--- Task_2.2/AQA_MTS_FinTech/Task_2/src/Cat.java
+++
@@ -1,8 +0,0 @@
-import java.time.LocalDate;
-
-public class Cat extends Pet {
-    public Cat(String breed, String name, Double cost, String character, LocalDate birthDate) {
-        super(breed, name, cost, character, birthDate);
-    }
-
-}
--- Task_2.2/AQA_MTS_FinTech/Task_2/src/Wolf.java
+++
@@ -1,7 +0,0 @@
-import java.time.LocalDate;
-
-public class Wolf extends Predator {
-    public Wolf(String breed, String name, Double cost, String character, LocalDate birthDate) {
-        super(breed, name, cost, character, birthDate);
-    }
-}
